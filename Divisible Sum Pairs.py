32f773c8c28316ebb8453e782da4158a1b04998f
TANISHKA GUPTA <84371065+thetanishkagupta@users.noreply.github.com>
2021-09-05 13:21:48 +0530

    Update Divisible Sum Pairs.py

--- Divisible Sum Pairs.py
+++ Divisible Sum Pairs.py
@@ -1,1 +1,10 @@
-
+n, k = input().split()
+n = int(n)
+k = int(k)
+ar = list(map(int,input().strip().split()))[:n]
+count=0
+for i in range(n):
+    for j in range(i+1, n):
+        if(ar[i]+ar[j]) % k == 0:
+            count += 1
+print(count)          
