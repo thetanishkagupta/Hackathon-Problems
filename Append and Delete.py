dbf2b38ebef599ccba743204b1a1934ad7536631
TANISHKA GUPTA <84371065+thetanishkagupta@users.noreply.github.com>
2021-09-11 16:59:01 +0530

    Create Append and Delete.py

---
+++ Append and Delete.py
@@ -0,0 +1,18 @@
+s = input()
+t = input()
+k = int(input().strip())
+count = 0
+#for loop for calculating the matching characters of both the string
+for i,j in zip(s, t):  #iterate each character of s and t in i and j 
+    if i == j:
+        count += 1
+    else:
+        break
+        
+total_length = len(s) + len(t) #length of both the string
+
+if (total_length<= 2*count+k and total_length%2==k%2 or total_length<k):
+    print("Yes")
+else:
+    print("No")
+    
